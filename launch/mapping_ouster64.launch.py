01170458b83e61c1edbd9a8a51ddfb17fb0de5dc
LihanChen2004 <757003373@qq.com>
2024-04-25 22:04:06 +0800

    🎈 perf: Launch params align with new branch

--- launch/mapping_ouster64.launch.py
+++ launch/mapping_ouster64.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
             'space_down_sample': True,
             'filter_size_surf': 0.5,  # Options: 0.5, 0.3, 0.2, 0.15, 0.1
             'filter_size_map': 0.5,  # Options: 0.5, 0.3, 0.15, 0.1
-            'cube_side_length': 1000.0,  # Option: 1000 (changed from 2000)
+            'ivox_nearby_type': 6,  # Options: 0.5, 0.3, 0.15, 0.1
             'runtime_pos_log_enable': False,  # Option: True
         }
     ]
